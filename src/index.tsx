fc8af17c5f3c8aed40be48298f91893946bf78b3
evgenyshipko <evgeny-shipko@yandex.ru>
2021-04-06 11:33:46 +0700

    add constants to export

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { ShipTable } from './components/ShipTable';
+import { COLUMN_TYPE, SEARCH_TYPE, SORT_TYPE } from './constants/C';
 
 export * from './types/PublicTypes';
-export { ShipTable };
+export { ShipTable, COLUMN_TYPE, SEARCH_TYPE, SORT_TYPE };
