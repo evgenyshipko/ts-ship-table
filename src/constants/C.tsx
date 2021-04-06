f00a70de7eb7e550a0557a55479975a8cacf87a4
evgenyshipko <evgeny-shipko@yandex.ru>
2021-04-06 12:52:13 +0700

    fix types

--- src/constants/C.tsx
+++ src/constants/C.tsx
@@ -1,20 +1,22 @@
+import { ColumnValueType, SearchMode, SortMode } from '..';
+
 class C {
     static DATE_FORMAT = 'YYYY-MM-DD';
     static FILTER_ROW_ID = 'filter';
 }
 export default C;
 
-export const SEARCH_TYPE = {
+export const SEARCH_TYPE: Record<string, SearchMode> = {
     FRONT: 'front',
     BACK: 'back',
 };
 
-export const SORT_TYPE = {
+export const SORT_TYPE: Record<string, SortMode> = {
     FRONT: 'front',
     BACK: 'back',
 };
 
-export const COLUMN_TYPE = {
+export const COLUMN_TYPE: Record<string, ColumnValueType> = {
     DATE: 'date',
     NUMBER: 'number',
     TEXT: 'text',
